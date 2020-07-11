0c54f08823bf118dba2d58156d3b17b381069bf6
Premananth V <premspace@gmail.com>
2020-07-11 22:14:46 +0100

    Updates

---
+++ precache-manifest.153e2c48a3b2ae40242d3a78da2118fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed374796434b428850ad56f907126457",
+    "url": "/sample-ui/index.html"
+  },
+  {
+    "revision": "c743daea65a7770d2f46",
+    "url": "/sample-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2637d9160d253d9a95d",
+    "url": "/sample-ui/static/js/2.5eecc3ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-ui/static/js/2.5eecc3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c743daea65a7770d2f46",
+    "url": "/sample-ui/static/js/main.3e9fbb2d.chunk.js"
+  },
+  {
+    "revision": "35207c49aef229db69c5",
+    "url": "/sample-ui/static/js/runtime-main.f12c1e3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
